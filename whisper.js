15cb8f9024aedb5b54fce2c77fabe424b4b88325
Draeyo <draeyo@gmail.com>
2016-10-30 03:04:49 +0100

    update

--- whisper.js
+++ whisper.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
 
+var token = 'MjM4NzQ3Nzk2NTI4ODI0MzI4.CvbBmg.dsMntCwqkTEtRd4qlQ_83KTgisc'
 var prefix = 'w ';
 
 bot.on('ready', () => {
@@ -8,12 +9,46 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-	if (msg.content.toLowerCase() === prefix + 'ping') {
-		msg.reply('pong', { split: true });
+	var input = msg.content.toLowerCase();
+	var user = msg.author.username;
+	var parsed = msg.content.split(" ");
+	var channel = msg.channel.name;
+
+	if (input === prefix + 'ping') {
+		msg.channel.sendMessage('pong', { split: true })
+			.then(msg => console.log('Sent a reply to ' + user + ' in channel ' + channel))
+			.catch(console.error);
 	}
-	else if (msg.content.toLowerCase() === prefix + 'hi') {
-		msg.reply('hey there!', { split: true });
+	else if (input === prefix + 'hi') {
+		msg.reply('Hey there!', { split: true })
+			.then(msg => console.log('Sent a reply to ' + user + ' in channel ' + channel))
+			.catch(console.error);
+	}
+	else if (input === prefix + 'test') {
+		msg.reply('OK', { split: true })
+			.then(msg => console.log('Sent a reply to ' + user + ' in channel ' + channel))
+			.catch(console.error);
+	}
+	else if (input === prefix + 'avatar') {
+		msg.reply(msg.author.avatarURL)
+			.then(msg => console.log('Sent avatar URL to ' + user + ' in channel ' + channel))
+			.catch(console.error);
+	}
+	else if (input === prefix + 'bdo') {
+		msg.reply('http://imgur.com/a/6awSk', { split: true })
+			.then(msg => console.log('Sent bdo url to ' + user + ' in channel ' + channel))
+			.catch(console.error);
+	}
+	else if (input === prefix + 'fs') {
+		msg.reply('http://i.imgur.com/7bfkYan.png123abc44', { split: true })
+			.then(msg => console.log('Sent fs file to ' + user + ' in channel ' + channel))
+			.catch(console.error);
+	}
+	else if (input === prefix + 'help') {
+		msg.channel.sendMessage('ping\nhi\ntest\navatar\nbdo\nfs\nhelp', { split: true })
+			.then(msg => console.log('User ' + user + ' asked for help in channel ' + channel))
+			.catch(console.error);
 	}
 });
 
-bot.login('MjM4NzQ3Nzk2NTI4ODI0MzI4.CvbBmg.dsMntCwqkTEtRd4qlQ_83KTgisc');
+bot.login(token);
